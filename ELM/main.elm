7171014fc0d17162e9e06c518d7979ee0c3771ad
Xuan <zxuuuan0321@gmail.com>
2024-01-19 10:39:09 +0100

    modified:   ELM/elm-stuff/0.19.1/d.dat 	modified:   ELM/index.html 	modified:   ELM/main.elm

--- ELM/main.elm
+++ ELM/main.elm
@@ -1,5 +1,19 @@
 module Main exposing (..)
-import Html
+import Html exposing (..)
+
 
 main =
-    Html.text "Guess it!"
+  div []
+    [ h1 [] [ text "My Grocery List" ]
+    , ul []
+        [ li [] [ text "Black Beans" ]
+        , li [] [ text "Limes" ]
+        , li [] [ text "Greek Yogurt" ]
+        , li [] [ text "Cilantro" ]
+        , li [] [ text "Honey" ]
+        , li [] [ text "Sweet Potatoes" ]
+        , li [] [ text "Cumin" ]
+        , li [] [ text "Chili Powder" ]
+        , li [] [ text "Quinoa" ]
+        ]
+    ]
