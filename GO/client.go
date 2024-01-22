30edcbe9777bbdca6a75e8e06548acbc1f94bd89
Xuan <zxuuuan0321@gmail.com>
2024-01-22 15:39:07 +0100

    modified:   GO/client.go 	modified:   GO/fonctions.go 	modified:   GO/server.go

--- GO/client.go
+++ GO/client.go
@@ -1,27 +1,59 @@
-// client.go
-
 package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
+	conn, err := net.Dial("tcp", "localhost:8888")
 	if err != nil {
 		fmt.Println("Error connecting:", err)
 		return
 	}
 	defer conn.Close()
 
-	// 读取服务器的响应
+	// Read server response
 	buffer := make([]byte, 1024)
 	n, err := conn.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading:", err)
 		return
 	}
+	fmt.Println(string(buffer[:n]))
 
-	fmt.Println("Server says:", string(buffer[:n]))
+	for {
+		// Get command from user
+		fmt.Print("Enter command: 1->gray ; 2->blur ; 3->reverse ; 4->disconnect: ")
+		var cmd string
+		fmt.Scanln(&cmd)
+
+		switch cmd {
+		// Send command to server
+		case "1", "2", "3":
+			_, err := conn.Write([]byte(cmd))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		// Disconnect
+		case "4":
+			_, err := conn.Write([]byte(cmd))
+			if err != nil {
+				log.Fatal(err)
+			}
+			// Wait for server to acknowledge the disconnect
+			n, err = conn.Read(buffer)
+			fmt.Print("Disconnecting......")
+			if err != nil {
+				log.Fatal("Error reading:", err)
+			}
+			return
+
+		// Enter correct choice
+		default:
+			fmt.Print("Invalid choice")
+		}
+	}
 }
--- GO/server.go
+++ GO/server.go
@@ -1,29 +1,59 @@
-// server.go
-
 package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	// 处理连接逻辑，这里简单回复一条消息
-	message := "Hello from the server!"
-	conn.Write([]byte(message))
+	for {
+		// Send a message to client
+		message := "Connexion OK"
+		conn.Write([]byte(message))
+
+		// Listen commands from client
+		buffer := make([]byte, 1024)
+		n, err := conn.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading:", err)
+			}
+			break
+		}
+
+		// Get commands from client
+		command := string(buffer[:n])
+
+		// Deal with commands
+		switch command {
+		case "1":
+			Traitement_griser()
+
+		case "2":
+			Traitement_flouter()
+
+		case "3":
+			Traitement_inverser()
+
+		case "4":
+			fmt.Println("Client requested to close the connection")
+			return
+		}
+	}
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8080")
+	listener, err := net.Listen("tcp", "localhost:8888")
 	if err != nil {
 		fmt.Println("Error listening:", err)
 		return
 	}
 	defer listener.Close()
 
-	fmt.Println("Server listening on localhost:8080")
+	fmt.Println("Server listening on localhost:8888")
 
 	for {
 		conn, err := listener.Accept()
@@ -34,6 +64,4 @@ func main() {
 
 		go handleConnection(conn)
 	}
-
-	Traitement()
 }
